f9cdb50ac16009c5c6fdb21acd0175ab8c322d3f
Ebesoh <ebesoh82@gmail.com>
2025-10-28 10:28:54 +0100

    Divide function and test added

---
+++ functions/Divide.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace calculator.functions
+{
+    public class Divide
+    {
+        public static int DivideNumbers(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ tests/Divide.test.cs
@@ -0,0 +1,38 @@
+using NUnit.Framework;
+using calculator.functions;
+
+namespace calculator.tests
+{
+    public class DivideTests
+    {
+        [Test]
+        public void DivideNumbers_ValidInput_ReturnsQuotient()
+        {
+            Assert.That(Divide.DivideNumbers(6, 3), Is.EqualTo(2));
+        }
+
+        [Test]
+        public void DivideNumbers_ZeroDividend_ReturnsZero()
+        {
+            Assert.That(Divide.DivideNumbers(0, 5), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void DivideNumbers_NegativeDividend_ReturnsNegativeQuotient()
+        {
+            Assert.That(Divide.DivideNumbers(-6, 3), Is.EqualTo(-2));
+        }
+
+        [Test]
+        public void DivideNumbers_NegativeDivisor_ReturnsNegativeQuotient()
+        {
+            Assert.That(Divide.DivideNumbers(6, -3), Is.EqualTo(-2));
+        }
+
+        [Test]
+        public void DivideNumbers_BothNegative_ReturnsPositiveQuotient()
+        {
+            Assert.That(Divide.DivideNumbers(-6, -3), Is.EqualTo(2));
+        }
+    }
+}
